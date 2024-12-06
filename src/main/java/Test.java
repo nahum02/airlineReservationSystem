931395e42222bdfce7756dd573948184a232af14
omarm <omarumustafa@gmail.com>
2024-12-05 20:25:34 -0500

    Testing File1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hey this worked");
-        System.out.printf("I was able to push");
+        System.out.printf("I was able to push again");
 
     }
 }
